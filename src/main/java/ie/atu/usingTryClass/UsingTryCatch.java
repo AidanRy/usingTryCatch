e97dcc81601998f63495a635972d14c300950f86
Aidan <g00365056@atu.ie>
2022-11-08 13:39:43 +0000

    started code before full code

---
+++ src/main/java/ie/atu/usingTryClass/UsingTryCatch.java
@@ -0,0 +1,12 @@
+package ie.atu.usingTryClass;
+
+import java.util.Scanner;
+
+public class UsingTryCatch
+{
+    public static void main(String[] args) {
+        System.out.println("please enter a number between 0 - 9 :");
+        Scanner myscan = new Scanner(System.in);
+        int myNum =myscan.nextInt();
+    }
+}
