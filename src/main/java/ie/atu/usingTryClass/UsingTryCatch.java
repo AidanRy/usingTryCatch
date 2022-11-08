fa15e0381a0828f265199c83bcf08e73857dcdef
Aidan <g00365056@atu.ie>
2022-11-08 14:06:22 +0000

    added try and catch plus while loop.

--- src/main/java/ie/atu/usingTryClass/UsingTryCatch.java
+++ src/main/java/ie/atu/usingTryClass/UsingTryCatch.java
@@ -1,12 +1,29 @@
 package ie.atu.usingTryClass;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class UsingTryCatch
 {
     public static void main(String[] args) {
-        System.out.println("please enter a number between 0 - 9 :");
+
         Scanner myscan = new Scanner(System.in);
-        int myNum =myscan.nextInt();
+        char repeat = 'y';
+        while(repeat == 'y') {
+            System.out.println("please enter a number between 0 - 9 :");
+            try {
+                int myNum = myscan.nextInt();
+
+                if (myNum <= 9 && myNum > 0) {
+                    System.out.println("you entered : " + myNum);
+                    repeat = 'n';
+                } else {
+                    System.out.println("Not a valid number ");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Not a valid input ");
+                myscan.next();
+            }
+        }
     }
 }
